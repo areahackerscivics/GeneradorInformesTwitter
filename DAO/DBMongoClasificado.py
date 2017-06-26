173f4caae3725eda0bf75eaa3d8432912271e91c
marymatt <marylin.mattos@gmail.com>
2017-06-26 11:04:50 +0200

    Incluir nuevos métodos a los DAO

--- DAO/DBMongoClasificado.py
+++ DAO/DBMongoClasificado.py
@@ -1,13 +1,11 @@
 #!/usr/bin/env python
 # encoding: utf-8
 
-#import jsonpickle
-#import tweepy
 import random
-#import datetime
 from datetime import datetime
 from pymongo import MongoClient#Libreria Mongodb
 
+#REAL
 from conexionMongo import *
 
 #Conexion a MongoDB
@@ -17,6 +15,11 @@ tdb = getDB()
 db = client[tdb]
 coleccion = getCollTweetsClas()
 tweetsdb = db[coleccion]
+
+#Declaración de variables
+
+today=datetime.now()
+dicc={}
 
 def actualizar_textoclasificados(idt,estado):
     try:
@@ -28,9 +31,8 @@ def actualizar_textoclasificados(idt,estado):
     except:
         print "No se pudo actualizar"
 
-def guardar_textoclasificados(corpus,puntaje,clases,idt):
+def guardar_textoclasificados(corpus,puntaje,clases,idt,fechaTweet):
     y_pred=[]
-    today=datetime.now()
     for i in range(len(corpus)):
         lista= sorted(zip(puntaje[i],  clases), reverse=True)
         y_pred.append(lista[0][1])#lista de las categorias predicas, acertadas y erradas
@@ -39,7 +41,8 @@ def guardar_textoclasificados(corpus,puntaje,clases,idt):
             "puntaje":lista[0][0],
             "idt":idt[i], #1
             "texto":corpus[i].decode('utf-8'),
-            "fecha":today
+            "fecha":today,
+            "fechaTweet":fechaTweet[i]
             };
         try:
             tweetsdb.insert_one(guardar)#Almacenar #1
@@ -66,47 +69,30 @@ def leer_textoclasificadoUno():
         texto.append(str(text['texto'].encode('utf-8')))
     return idt,categoria,texto
 
-def leer_textoclasificadoTodo(cat, num, fechaini, fechafin):
+def leer_textoclasificadoTodo(cat, num, fechaini, fechafin):#para la ventana Revision
     idt=[]
     categoria=[]
     texto=[]
+    fechaTweet=[]
     fechaini=datetime.strptime(fechaini,"%Y-%m-%d")
     fechafin=datetime.strptime(fechafin,"%Y-%m-%d")
     for text in tweetsdb.find({"estado": {"$exists":False},"categoria":cat,"fecha":{ "$gt" :fechaini ,"$lt" :fechafin}},
-                              {"idt":1,"categoria":1,"texto":1,"_id":0}).limit(int(num)):
+                              {"idt":1,"categoria":1,"texto":1,"fechaTweet":1,"_id":0}).limit(int(num)):
         idt.append(str(text['idt']))
         categoria.append(str(text['categoria'].encode('utf-8')))
         texto.append(str(text['texto'].encode('utf-8')))
-    return idt,categoria,texto
+        fechaTweet.append(str(text['fechaTweet']))
+    return idt,categoria,texto,fechaTweet
 
-def leer_textoclasificadoxfecha():#para la pantalla clasificar
-    fechaClasificacion=[]
-    fechaTweet=[]
-    total=[]
 
-    pipeline=[{
-      "$lookup":
-        {
-          "from": "tweets",
-          "localField": "idt",
-          "foreignField": "idt",
-          "as": "tweets_docs"
-        }
-   },
-   {"$project":
-       {
-           "fechaTweet": "$tweets_docs.fechaDescarga" ,"_id":0,
-           "fechaClasificacion": { "$dateToString": { "format": "%Y-%m-%d", "date": "$fecha" } } }
-   },
-  {"$group":{"_id":{"fechaClasificacion":"$fechaClasificacion","fechaTweet":"$fechaTweet"},"total":{"$sum":1}}},
-  {"$project":
-       {
-           "fechaClasificacion":"$_id.fechaClasificacion",
-           "fechaTweet":"$_id.fechaTweet",
-           "total":"$total",
-           "_id":0
-           }}]
+def leer_Agrupadoxfecha():#para la ventana Clasificar
+    dicc={}
+    pipeline=[{"$project": {"_id":0,
+                            "fecha":1,
+                            "fechaTweet": { "$dateToString": { "format": "%Y-%m-%d", "date": "$fechaTweet" } } }},
+              {"$group":{"_id":{"fechaTweet":"$fechaTweet"},"total":{"$sum":1},"fechaDescargaMax": {"$max":"$fecha"}}},
+              {"$project": {"_id":0,"fechaTweet":"$_id.fechaTweet","fechaDescargaMax":1, "Total_Tweets":"$total"}},
+              {"$sort":{"fechaTweet":1}}]
     for text in tweetsdb.aggregate(pipeline):
-        fechaClasificacion.append(text['fechaClasificacion'])
-        fechaTweet.append(text['fechaTweet'][0])
-        total.append(text['total'])
+         dicc[text['fechaTweet']]=[text['Total_Tweets'],text['fechaDescargaMax']]
+    return dicc
--- DAO/DBMongoTweet.py
+++ DAO/DBMongoTweet.py
@@ -5,6 +5,7 @@ import time
 from datetime import datetime
 from pymongo import MongoClient#Libreria Mongodb
 
+#REAL
 from conexionMongo import *
 
 #Conexion a MongoDB
@@ -15,19 +16,30 @@ db = client[tdb]
 coleccion = getCollTweets()
 tweetsdb = db[coleccion]
 
-#Conexion a MongoDB
-cliente = MongoClient()#Inicializar objeto
-cliente = MongoClient('127.0.0.1', 27017)#Indicar parametros del servidor
-bd = cliente.twitter#Seleccionar Schema
-tweetsdb = bd.tweets#Seleccionar Coleccion para buscar
-
-idt=[]
-tweet=[]
+#Declaración de variables
+dicc={}
 def leer(fechaini,fechafin):
+    idt=[]
+    tweet=[]
+    fechaTweet=[]
     fechaini=datetime.strptime(fechaini,"%Y-%m-%d")
     fechafin=datetime.strptime(fechafin,"%Y-%m-%d")
     #for text in tweetsdb.find({"idioma":"es","consulta": "@AjuntamentVLC", "fechaDescarga":"22-03-17"},{"idt":1,"tweet":1,"_id":0}) :
-    for text in tweetsdb.find({"idioma":"es","consulta": "@AjuntamentVLC", "fecha":{ "$gt" :fechaini ,"$lt" :fechafin}},{"idt":1,"tweet":1,"_id":0}) :
+    for text in tweetsdb.find({"idioma":"es","consulta": "@AjuntamentVLC", "fechaTweet":{ "$gt" :fechaini ,"$lt" :fechafin}},{"idt":1,"tweet":1,"fechaTweet":1,"_id":0}) :
         idt.append(str(text['idt']))
         tweet.append(str(text['tweet'].encode('utf-8')))
-    return idt,tweet
+        fechaTweet.append(text['fechaTweet'])
+    return idt,tweet,fechaTweet
+
+def leer_AgrupadoxfechaTW():#para la ventana Clasificar
+    dicc={}
+    pipeline=[{"$match":{"consulta": "@AjuntamentVLC", "idioma":"es",}},
+              {"$project": {"_id":0,
+                            "fechaTweet": { "$dateToString": { "format": "%Y-%m-%d", "date": "$fechaTweet" } } }},
+              {"$group":{"_id":{"fechaTweet":"$fechaTweet"},"total":{"$sum":1}}},
+              {"$project": {"_id":0,"fechaTweet":"$_id.fechaTweet", "Total_Tweets":"$total"}},
+              {"$sort":{"fechaTweet":1}}]
+    for text in tweetsdb.aggregate(pipeline):
+        dicc[text['fechaTweet']]=text['Total_Tweets']
+    return dicc
+    
